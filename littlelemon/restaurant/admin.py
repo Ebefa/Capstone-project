34eba6b5879db377d287bc0dcbe0252d07e9a44a
Kwaku <KwakuKwatiah@gmail.com>
2024-03-06 21:30:26 +0000

    third

--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Booking_Table
+from .models import Menu_Table
+
+
+admin.site.register(Booking_Table)
+admin.site.register(Menu_Table)
--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Booking_Table(models.Model):
+    name = models.CharField(max_length=200)
+    Id = models.IntegerField()
+    No_of_Guests = models.IntegerField()
+    BookingDate = models.DateField()
+
+
+def __str__(self):
+    return self.name
+
+class Menu_Table(models.Model):
+    Id = models.IntegerField()
+    Title= models.CharField(max_length=200)
+    Price= models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField()
+
